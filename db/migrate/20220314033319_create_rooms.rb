6ba1ef9667e7ae94d6b83f82aca1f5584e425a15
anonymous anonymous <avery.marcus@kellogg.northwestern.edu>
2022-03-14 03:33:20 +0000

    Generate rooms resource with ideas:scaffold

---
+++ db/migrate/20220314033319_create_rooms.rb
@@ -0,0 +1,11 @@
+class CreateRooms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :rooms do |t|
+      t.string :room_name
+      t.integer :home_id
+      t.string :style_name
+
+      t.timestamps
+    end
+  end
+end
