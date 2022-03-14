c0f82fb4e23940000af067870d8d4f71b72a2100
anonymous anonymous <avery.marcus@kellogg.northwestern.edu>
2022-03-14 03:35:01 +0000

    Remove controller comments

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,26 +1,21 @@
 class HomesController < ApplicationController
   before_action :set_home, only: %i[show edit update destroy]
 
-  # GET /homes
   def index
     @q = Home.ransack(params[:q])
     @homes = @q.result(distinct: true).includes(:rooms).page(params[:page]).per(10)
   end
 
-  # GET /homes/1
   def show
     @room = Room.new
   end
 
-  # GET /homes/new
   def new
     @home = Home.new
   end
 
-  # GET /homes/1/edit
   def edit; end
 
-  # POST /homes
   def create
     @home = Home.new(home_params)
 
@@ -31,7 +26,6 @@ class HomesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /homes/1
   def update
     if @home.update(home_params)
       redirect_to @home, notice: "Home was successfully updated."
@@ -40,7 +34,6 @@ class HomesController < ApplicationController
     end
   end
 
-  # DELETE /homes/1
   def destroy
     @home.destroy
     redirect_to homes_url, notice: "Home was successfully destroyed."
@@ -48,12 +41,10 @@ class HomesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_home
     @home = Home.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def home_params
     params.require(:home).permit(:owner, :home_name)
   end
