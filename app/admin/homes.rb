5c129d0d2fe21894a444a53667bd5d0dc194a2b3
anonymous anonymous <avery.marcus@kellogg.northwestern.edu>
2022-03-14 03:33:42 +0000

    Whitelist columns in Home for ActiveAdmin

--- app/admin/homes.rb
+++ app/admin/homes.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Home do
 
+ permit_params :owner, :home_name
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
