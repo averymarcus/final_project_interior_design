17098c0e734d2ec59137416acaa9984c512b7c5e
anonymous anonymous <avery.marcus@kellogg.northwestern.edu>
2022-03-14 03:34:12 +0000

    Generate graphiti resources for furnitures

---
+++ app/resources/furniture_resource.rb
@@ -0,0 +1,13 @@
+class FurnitureResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :room_id, :integer
+  attribute :image, :string
+  attribute :furniture_name, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
