28b8e2247eecc5563e4cde96c669ef3cd2e4537b
anonymous anonymous <avery.marcus@kellogg.northwestern.edu>
2022-03-14 03:34:14 +0000

    Generate graphiti resources for homes

---
+++ app/resources/home_resource.rb
@@ -0,0 +1,12 @@
+class HomeResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :owner, :string
+  attribute :home_name, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
